bbe6791d22b1846803d25dd2e25493f1e27a1737
Yuri Numerov <achi@hush.ai>
2015-01-16 01:25:17 +0100

    removed getters and setters, improved assignment syntax

--- BinaryTree.py
+++ BinaryTree.py
@@ -4,43 +4,28 @@ class BinaryTree():
   """
   
   def __init__(self,val):
+
     self._value = val
-    self._left = None
-    self._right = None
-    self._parent = None
+    self.left = None
+    self.right = None
+    self.parent = None
   
   def __repr__(self):
+
     return str(self.inorder())
 
-
   def set_left(self,node):
-    self._left = node
-    if not isinstance(node, BinaryTree):
-      self._left = BinaryTree(node)
-    self._left._parent = self
+
+    self.left=BinaryTree(node) if not isinstance(node, BinaryTree) else node
+    self.left.parent = self
 
   def set_right(self,node):
-    self._right = node
-    if not isinstance(node, BinaryTree):
-      self._right = BinaryTree(node)
-    self._right._parent = self
+
+    self.right = BinaryTree(node) if not isinstance(node, BinaryTree) else node
+    self.right.parent = self
 
   def inorder(self):
-    if self._left is not None:
-      left = self._left.inorder()
-    else:
-      left = []
-    if self._right is not None:
-      right = self._right.inorder()
-    else:
-      right = []
+ 
+    left  = self.left.inorder()  if self.left  is not None else left  = []
+    right = self.right.inorder() if self.right is not None else right = []                      
     return filter(None,[left,[self._value],right]) 
-
-  def right(self):
-    return self._right
-
-  def left(self):
-    return self._left
-
-  def parent(self):
-    return self._parent
